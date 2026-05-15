55879611c5a8e4e21ba40c082daeab7b42e6b7a4
imchrm <im.chrm@gmail.com>
2026-05-15 18:47:57 +0500

    feat: added new appartment tour

--- src/data/tours.js
+++ src/data/tours.js
@@ -21,10 +21,10 @@ export const TOURS = [
     typeKey: 'real_estate',
   },
    {
-    id: 'neo_1',
-    embedSrc: 'tours/neo_1/index.html',
-    previewSrc: 'tours/neo_1/tiles/0-bedroom-left/preview.jpg',
-    titleKey: 'tours.neo_1.title',
+    id: 'appartment_1',
+    embedSrc: 'tours/appartment_1/index.html',
+    previewSrc: 'tours/appartment_1/tiles/0-hall/preview.jpg',
+    titleKey: 'tours.appartment_1.title',
     typeKey: 'real_estate',
   },
    {
