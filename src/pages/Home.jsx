2dee676e4b03f86ca97bcb272f04becfce185ff8
imchrm <im.chrm@gmail.com>
2026-05-19 19:36:21 +0500

    feat: chaged apartment tour

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="pt-24">
+      <main className="pt-16">
         <Hero />
         <AboutCards />
         <Services />
